add0143650de3df91a3ea996bf4721bbd405bcda
whoisaf <59835773+whoisaf@users.noreply.github.com>
2020-01-17 19:18:12 -0500

    deleted uneeded page data

--- client/src/data/landing.ts
+++
@@ -1,8 +0,0 @@
-import config from "../constants/config";
-
-export default {
-  title: "",
-  descrip: "",
-  heading: `<strong class="text-secondary">${config.appName}</strong>`,
-  subheading: `Insert really cool catch phrase here`
-};
