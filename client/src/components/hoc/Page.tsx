34126164c7db4aeefc9727b185e635a042342e08
whoisaf <59835773+whoisaf@users.noreply.github.com>
2020-01-21 23:18:16 -0500

    Update Page.tsx

--- client/src/components/hoc/Page.tsx
+++ client/src/components/hoc/Page.tsx
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { Helmet } from "react-helmet";
 import { useLocation } from "react-router-dom";
-import { CSSTransition } from "react-transition-group";
 
 import "./Page.scss";
 
